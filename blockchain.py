0d163f1d8cc6e47563d49e0dd8cdb48ccfeb7cf1
Kam Popat <kampopat@gmail.com>
2017-12-02 12:52:07 +0000

    Init Blockchain with a genesis block, create new blocks and hash blocks

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,37 @@
+import hashlib 
+import json 
+import time from time
+
 class Blockchain(object):
     def __init__(self):
         self.chain = [] 
         self.current_transactions = []
 
-    def new_block(self): 
-        # Creates a new block and adds it to the chain 
-        pass 
+        # Create the genesis block 
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None): 
+        """
+        Create a new Block in the Blockchain
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(), 
+            'transactions': self.current_transactions, 
+            'proof': proof, 
+            'previous_hash': previous_hash or self.hash(self.chain[-1]), 
+        }
+
+        # Reset the current list of transactions 
+        self.current_transactions = [] 
+
+        self.chain.append(block)
+        return block 
+        
 
     def new_transaction(self, sender, recipient, amount):
         """
@@ -26,11 +52,17 @@ class Blockchain(object):
 
     @staticmethod 
     def hash(block):
-        # Hashes a block 
-        pass 
+        """
+        Creates a SHA-256 hash of a Block
+        :param block: <dict> Block
+        :return: <str>
+        """
+        
+        # Make sure that the Dictionary is Ordered, otherwise we get inconsistent hashes
+         
 
     @property 
     def last_block(self):
         # Returns the last Block in the chain 
-        pass 
+        return self.chain[-1] 
 
